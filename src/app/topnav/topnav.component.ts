567969e85e2826e3fbf0f712fdb1d6d3971b226e
Pavel <pavelstn@gmail.com>
2017-02-22 12:10:54 +0300

    update at 201702221210

--- src/app/topnav/topnav.component.ts
+++ src/app/topnav/topnav.component.ts
@@ -2,14 +2,18 @@
  * Created by pavel on 22.02.17.
  */
 import {Component, Input} from '@angular/core';
-
+import { MainPageService} from '../services/mainapge.service';
+import 'rxjs/add/operator/map';
 @Component({
     selector: 'topnav',
-
     template: require('./topnav.component.html'),
-
+    providers: [MainPageService]
 })
 
 export class TopNavComponent{
-    constructor(){}
+    data={};
+    constructor(private mainPageService: MainPageService){}
+    ngOnInit(){
+        this.mainPageService.getData().subscribe((data)=>this.data=data);
+    }
 }
