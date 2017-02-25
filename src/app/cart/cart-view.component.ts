94c626b67574eb6870cf6a370d41b1dc5260ba16
Pavel <pavelstn@gmail.com>
2017-02-25 23:45:49 +0300

    update at 201702252345

--- src/app/cart/cart-view.component.ts
+++ src/app/cart/cart-view.component.ts
@@ -5,6 +5,7 @@ import {Component} from '@angular/core';
 import { MainPageService} from '../services/mainapge.service';
 import 'rxjs/add/operator/map';
 import { CartStoreService} from '../services/cart-store.service';
+import { OrderForm} from './order-form';
 
 @Component({
     selector: 'cart',
@@ -14,7 +15,11 @@ import { CartStoreService} from '../services/cart-store.service';
 
 export class CartViewComponent {
     cartData={};
+    orderForm:OrderForm;
+    showOrderForm:boolean;
     constructor(private cartStoreService:CartStoreService){
+        this.orderForm= new OrderForm();
+        this.showOrderForm= false;
         this.cartStoreService.cartChange.subscribe((cartList)=>{
             this.cartData= cartList;
             console.log("this.cartData", this.cartData);
@@ -39,5 +44,16 @@ export class CartViewComponent {
 
         console.log("sdsd",id, n);
         this.cartStoreService.changeNumber(id, n);
+    }
+
+    open_create_order_dialog(){
+        this.showOrderForm= true;
+    }
+    update_values(){
+        this.orderForm.checkValidateForm();
+    }
+
+    create_order_dialog(){
+
     }
 }
