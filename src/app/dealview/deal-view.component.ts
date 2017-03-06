436c984cce66bea25102e804ef3bda10486a31e2
Pavel <pavelstn@gmail.com>
2017-03-06 22:21:58 +0300

    update at 201703052221

--- src/app/dealview/deal-view.component.ts
+++ src/app/dealview/deal-view.component.ts
@@ -5,6 +5,7 @@ import {Component} from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
 import {Subscription} from 'rxjs/Subscription';
 import {DealDataService} from '../services/deal-data.service';
+import { MainPageService} from '../services/mainapge.service';
 import { CarouselModule } from 'ng2-bootstrap';
 import { CartStoreService} from '../services/cart-store.service';
 import { Title }     from '@angular/platform-browser';
@@ -18,23 +19,31 @@ import { Title }     from '@angular/platform-browser';
 export class DealViewComponent {
     id: number;
     dealData={};
+    dealsList={};
 
     private subscription: Subscription;
 
-    constructor(private titleService: Title, private activateRoute: ActivatedRoute, private dealDataService:DealDataService, private cartStoreService:CartStoreService ){
+    constructor(private titleService: Title, private activateRoute: ActivatedRoute, private dealDataService:DealDataService, private cartStoreService:CartStoreService , private mainPageService: MainPageService){
         this.subscription = activateRoute.params.subscribe(params=>{
             this.id=params['id'];
             this.dealDataService.loadData(this.id).subscribe((data) => {
                 this.dealData=data;
                 this.titleService.setTitle( data.title );
             });
+
+
+
+
         });
 
 
     }
 
     ngOnInit(){
-
+        this.mainPageService.getData().subscribe((data) => {
+            this.dealsList=data;
+            // this.titleService.setTitle( data.name );
+        });
     }
 
     ngOnDestroy(){
