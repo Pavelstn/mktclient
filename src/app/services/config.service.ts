6ebead3245a67ee7f117859e0e2a5144b993ab02
Pavel P <pavelstn@gmail.com>
2017-12-29 00:14:18 +0300

    update at 201712290014

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -5,7 +5,7 @@ export class ConfigService {
 
   user_id = 1;
   shop_id = 2;
-   dataServer = 'http://51rose.ru';
+  dataServer = 'http://mkt2u.ru';
   // dataServer = 'http://localhost:8080';
   projetctName = 'SmartWatch';
 
