61a4d1831bc49b6173ea3cb9ec18dae116d1c011
Pavel P <pavelstn@gmail.com>
2017-12-28 23:58:07 +0300

    update at 201712282358

--- src/app/components/spash-screen/spash-screen.component.ts
+++ src/app/components/spash-screen/spash-screen.component.ts
@@ -7,13 +7,30 @@ import {SplashScreenService} from '../../services/splash-screen.service';
   styleUrls: ['./spash-screen.component.sass']
 })
 export class SpashScreenComponent implements OnInit {
-  public showSplash= true;
+  public showSplash = false;
+  public showAnimate = false;
+  public hideAnimate = false;
+
   constructor(private ss: SplashScreenService) {
   }
 
   ngOnInit() {
     this.ss.change.subscribe((data) => {
-      this.showSplash = data;
+      // this.showSplash = data;
+      // план таков: как появляется сигнал показать сплэш- сразу показываем его и запускаем анимацию показа
+      // как появился сигнал скрыть- запускаем анимацию скрытия, а по таймеру через x секунд закрываем сплэш
+      if (data) {
+        this.showSplash = true;
+        this.showAnimate = true;
+        this.hideAnimate = false;
+      } else {
+        setTimeout(() => {
+          this.showSplash = false;
+        }, 600);
+        this.hideAnimate = true;
+        this.showAnimate = false;
+      }
+
     });
 
   }
